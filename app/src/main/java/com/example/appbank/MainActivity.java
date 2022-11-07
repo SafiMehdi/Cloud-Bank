561f86fbf2b55f62d76ac9b0eac2fb60e4f39b8a
SafiMehdi <safii.elmehdi@gmail.com>
2022-11-07 13:06:18 +0100

    initial commit

---
+++ app/src/main/java/com/example/appbank/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.appbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton =(Button)findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activityChangeIntent = new Intent(MainActivity.this, ListView.class);
+                MainActivity.this.startActivity(activityChangeIntent);
+            }
+        });
+    }
+}

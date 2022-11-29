9eef642957530da011c57118d8637f44f712c3f7
SafiMehdi <safii.elmehdi@gmail.com>
2022-11-29 22:06:44 +0100

    Nsit ma pushit u hadchi b9a hna idk wach khdam

--- app/src/main/java/com/example/appbank/ListsView.java
+++ app/src/main/java/com/example/appbank/ListsView.java
@@ -2,7 +2,10 @@ package com.example.appbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListsView extends AppCompatActivity {
     ListView list;
@@ -39,5 +42,35 @@ public class ListsView extends AppCompatActivity {
         list=(ListView)findViewById(R.id.list);
         list.setAdapter(adapter);
 
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+                // TODO Auto-generated method stub
+                if(position == 0) {
+                    //code specific to first list item
+                    Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 1) {
+                    //code specific to 2nd list item
+                    Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+                else if(position == 2) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 3) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 4) {
+
+                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/appbank/MainActivity.java
+++ app/src/main/java/com/example/appbank/MainActivity.java
@@ -5,21 +5,67 @@ import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
+    EditText loginUsername,loginPassword;
+    boolean isAllFieldChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginButton =(Button)findViewById(R.id.loginButton);
+        loginUsername = (EditText)findViewById(R.id.loginUsername);
+        loginPassword = (EditText)findViewById(R.id.loginPassword);
+        loginButton =(Button)findViewById(R.id.loginButton);
+
+        /*loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                isAllFieldChecked = fieldsCheck();
+                if(isAllFieldChecked) {
+                        Toast.makeText(getApplicationContext(), "Login success", Toast.LENGTH_SHORT).show();
+                        Intent activityChangeIntent = new Intent(MainActivity.this, ListsView.class);
+                        MainActivity.this.startActivity(activityChangeIntent);
+                }
+            }
+        });*/
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent activityChangeIntent = new Intent(MainActivity.this, ListsView.class);
-                MainActivity.this.startActivity(activityChangeIntent);
+                    Toast.makeText(getApplicationContext(), "Login success", Toast.LENGTH_SHORT).show();
+                    Intent activityChangeIntent = new Intent(MainActivity.this, ListsView.class);
+                    MainActivity.this.startActivity(activityChangeIntent);
             }
         });
     }
+
+    /*private boolean fieldsCheck() {
+        if (loginUsername.length() == 0) {
+            loginUsername.setError("This field is required");
+            return false;
+        }
+
+        if (loginPassword.length() == 0) {
+            loginPassword.setError("Password is required");
+            return false;
+        } else if (loginPassword.length() < 8) {
+            loginPassword.setError("Password must be minimum 8 characters");
+            return false;
+        }
+
+        if(loginUsername.getText().toString().trim() != "Mehdi" && loginPassword.getText().toString().trim() != "1234567890"){
+            loginUsername.setError("Username & password doesn't match !");
+            return false;
+        }else{
+            loginUsername.setError(null);
+            return true;
+        }
+        // after all validation return true.
+
+    }*/
 }
